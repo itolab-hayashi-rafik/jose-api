dcc011c3fa0426e62f5264bede58eeb264b9e346
Masayuki <hayashi.masayuki@itolab.nitech.ac.jp>
2016-08-10 19:31:10 +0900

    [fix] fixed shell script

--- remote_forward.sh
+++ remote_forward.sh
@@ -7,6 +7,6 @@ REMOTE_PORT=3000
 
 for host in 10.1.0.{4..43};
 do
-    echo "localhost:${local_port} <-- gw.jose.jp <-- ${host}:${REMOTE_PORT}"
+    echo "localhost:${LOCAL_PORT} <-- gw.jose.jp <-- ${host}:${REMOTE_PORT}"
     ssh -o StrictHostKeyChecking=no -o ProxyCommand="ssh -i \"${GW_SECRET}\" gw.jose.jp -p 2360 -W %h:%p" -C -N -f -i "${NODE_SECRET}" -R ${LOCAL_PORT}:${host}:${REMOTE_PORT} ${host}
 done
